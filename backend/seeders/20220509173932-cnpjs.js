c40e8015299b25030159ebc0d97d97f0c3ab4c6b
Raph2ll <raphael.ph2ll@gmail.com>
2022-05-10 10:51:54 -0300

    feat: implementa mais seeds

--- backend/seeders/20220509173932-cnpjs.js
+++ backend/seeders/20220509173932-cnpjs.js
@@ -16,6 +16,12 @@ module.exports = {
           createdAt: '2020-10-29 21:20:33',
           updatedAt: '2020-10-29 21:20:33',
         },
+        {
+          cnpj: '00000000000003',
+          companyType: '1',
+          createdAt: '2020-10-29 21:20:33',
+          updatedAt: '2020-10-29 21:20:33',
+        },
       ], {});
   },
 
