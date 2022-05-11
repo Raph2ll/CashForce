911b7c0da636ae59a047514e1a9d3efc87d22f62
Raph2ll <raphael.ph2ll@gmail.com>
2022-05-11 13:52:18 -0300

    feat: configurando a primeira rota

---
+++ frontend/src/pages/Invoices/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './style.css';
+
+function Invoices() {
+  return (
+    <div>
+      <div className="sider-financiador">
+
+      </div>
+    </div>
+  );
+}
+
+export default Invoices;
