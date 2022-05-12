1c6a2086a8297679957a5147c559dea69e7d067e
Raph2ll <raphael.ph2ll@gmail.com>
2022-05-12 11:56:49 -0300

    feat: criando pagina de notas fiscais sidebar + main-content

--- frontend/src/pages/Invoices/index.js
+++ frontend/src/pages/Invoices/index.js
@@ -1,12 +1,32 @@
 import React from 'react';
-
+import CashForce from '../../icons/CashForce.png';
+import handShake from '../../icons/handshake.svg'
+import OrderTable from '../../components/OrderTable/OrderTable';
 import './style.css';
 
 function Invoices() {
   return (
-    <div>
-      <div className="sider-financiador">
-
+    <div className='container'>
+      <div className='sidebar-container'>
+      <img src={ CashForce } alt='this is a CashForce logo' />
+        <ul className='sidebar-container'>
+            <li>
+              <img src={ handShake } alt="HandShake SVG"></img>
+              Notas fiscais
+            </li>
+        </ul>
+      </div>
+      <div className='main-content'>
+        <div className='header-sistema'>
+        </div>
+        <div className='frame'>
+          <div className='handShake' >
+            <img src={ handShake } alt="HandShake SVG"></img>
+            <p>Notas fiscais</p>
+          </div>
+          <p className='proposal'>Visualize as notas fiscais que você tem.</p>
+        </div>
+          <OrderTable />
       </div>
     </div>
   );
